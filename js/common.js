d4302a18c5ba8246bdf8ffff6b63b9ad39a62534
Ex-Zy <serega1905@ukr.net>
2015-12-03 12:27:41 +0200

    upd map

--- js/common.js
+++ js/common.js
@@ -41,4 +41,46 @@ head.ready(function() {
 		slidesToShow: 4,
 		slidesToScroll: 1,
 	});
+
+	//MAP
+	var map;
+	function initMap() {
+
+		var myLatLng = {lat: 55.774824, lng: 37.612028};
+
+		var map = new google.maps.Map(document.getElementById('map'), {
+			zoom: 16,
+			center: myLatLng,
+			// mapTypeControl: false,
+			// streetViewControl: false,
+			scrollwheel: false,
+			// zoomControl : false,
+			disableDefaultUI: true,
+		});
+
+		var marker = new google.maps.Marker({
+			position: myLatLng,
+			map: map,
+			title: 'Click to zoom!',
+			icon: 'img/marker.png'
+		});
+
+
+		map.addListener('center_changed', function() {
+			// 3 seconds after the center of the map has changed, pan back to the
+			// marker.
+			window.setTimeout(function() {
+			  map.panTo(marker.getPosition());
+			}, 3000);
+		});
+
+		marker.addListener('click', function() {
+			map.setZoom(18);
+			map.setCenter(marker.getPosition());
+		});
+
+
+	}
+	initMap();
+
 });
